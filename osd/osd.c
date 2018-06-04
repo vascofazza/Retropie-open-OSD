00063d4f33a24641e5064fbc0154ff83ed6c86cc
vascofazza <fscozzafava@imacdifederico.lan>
2018-06-04 20:05:46 +0200

    shadow + gitignore

--- osd/osd.c
+++ osd/osd.c
@@ -176,8 +176,9 @@ void updateInfo(IMAGE_LAYER_T *infoLayer)
     snprintf(buffer, sizeof(buffer),"%04d-%02d-%02d %02d:%02d:%02d\nTemperature: %.1f\x5\x43\nBrigthness:  %d/%d\nBattery:     %d%%\nVoltage:     %.2fV\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, temp, brightness, BRIGHTNESS_MAX, battery, voltage/100.f);
 
     IMAGE_T *image = &(infoLayer->image);
-    clearImageRGB(image, &backgroundColour);
+    clearImageRGB(image, &clearColor);
     int x = 1, y = 1;
+    drawStringRGB(x+1, y, buffer, &backgroundColour, image);
     drawStringRGB(x, y, buffer, &textColour, image);
     changeSourceAndUpdateImageLayer(infoLayer);
 }
